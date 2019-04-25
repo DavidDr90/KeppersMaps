3d168ce8a5d5d31e43a28b1e79f64ff4a5961c6e
david <davidd.r.90@gmail.com>
2019-04-25 14:46:15 +0300

    clean the google maps component

--- src/app/google-maps/google-maps.component.ts
+++ src/app/google-maps/google-maps.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { HostListener } from "@angular/core"
 import { JsonService } from '../services/json.service';
-import { Marker, Subjects } from "../marker";
-import { AgmMap, LatLngBounds } from '@agm/core';
-
-import 'rxjs/add/operator/takeUntil';
-import { map, tap, takeUntil} from 'rxjs/operators';
+import { AgmMap} from '@agm/core';
+import { takeUntil} from 'rxjs/operators';
 import { Subject } from 'rxjs';
 
 
@@ -22,7 +18,6 @@ var _ = require('lodash');
 
 declare var google: any;
 const DEFUALT_LATITUDE = 51.5074, DEFUALT_LONGITUDE = 0.1278;//London UK
-const SMALL_ARRAY_CONST = 20;
 
 @Component({
   selector: 'app-google-maps',
@@ -32,278 +27,6 @@ const SMALL_ARRAY_CONST = 20;
 export class GoogleMapsComponent implements OnInit {
 
   @ViewChild('AgmMap') agmMap: AgmMap;
-
-  geoJsonObject = {
-    "type": "FeatureCollection",
-    "features": [
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.102996826171875,
-            51.54804306453371
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.067291259765625,
-            51.52754263322114
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.15106201171874997,
-            51.52241608253253
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.07415771484375,
-            51.49164465653034
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.10848999023437499,
-            51.52754263322114
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.1263427734375,
-            51.4830933498849
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.1318359375,
-            51.515579783755925
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            0.248565673828125,
-            51.60266574567797
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            0.254058837890625,
-            51.56768064709022
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            0.190887451171875,
-            51.61119461048402
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            0.08651733398437499,
-            51.386352101119364
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            0.1043701171875,
-            51.380352540777984
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            0.087890625,
-            51.376923867455886
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.34332275390625,
-            51.586456488215426
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.38864135742187494,
-            51.37520943448463
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.402374267578125,
-            51.36320660581431
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.372161865234375,
-            51.345196463050826
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.347442626953125,
-            51.376923867455886
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.11810302734375,
-            51.282534682474655
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.08651733398437499,
-            51.29627609493991
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.146942138671875,
-            51.28597042110153
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.11947631835937499,
-            51.31344707827587
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.124969482421875,
-            51.394921537317366
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -0.092010498046875,
-            51.39577839266317
-          ]
-        }
-      }
-    ]
-  }
-
-
 
   labelOptions = {
     color: 'black',
@@ -333,34 +56,14 @@ export class GoogleMapsComponent implements OnInit {
     },
   ]*/
 
-  //this array are use for display the markers on the map
-  heavy: any;
-  medium: any;
-  easy: any;
-
+ 
   data: any;
   mapsZoom: any;
-  mapBorders;
 
   lat: number = DEFUALT_LATITUDE;
   lng: number = DEFUALT_LONGITUDE;
-
-  //this hold all the date locally
-  markersArray = {
-    "heavy": [], "medium": [], "easy": []
-  };
-  leftover: any;
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event?) {
-    this.screenHeight = window.innerHeight;
-    this.screenWidth = window.innerWidth;
-    // console.log("height: " + this.screenHeight);
-    // console.log("width: " + this.screenWidth);
-  }
-
   userCurrentLocation: any;
-  markers = []//Promise<Marker[]>;// = [];
+
   // TODO: change the map css style to match the current screen size
   screenHeight;
   screenWidth;
@@ -370,8 +73,8 @@ export class GoogleMapsComponent implements OnInit {
 
 
   constructor(private jsonService: JsonService) {
-    console.log("constractor");
-    this.onResize();
+    // listen to changes in the json data from the menu component
+    // if there is data save it localy and display it on the map
     jsonService.data$
     .pipe(takeUntil(this.alive))
     .subscribe(
@@ -382,11 +85,17 @@ export class GoogleMapsComponent implements OnInit {
       });
   }
 
+
+  /** When the component is destory close 
+   *  the subscribe of the json data
+   *  To free all memory use
+   */
   ngOnDestroy() {
     console.log('ngOnDestory');
     this.alive.next();
     this.alive.complete();
   }
+
 
   /** on component init
    * ask the user for prumssion to get his location
@@ -419,6 +128,7 @@ export class GoogleMapsComponent implements OnInit {
     }
   }
 
+
   /** init the map using the input location
    * initiate the map on the page
    * zoom is the display distance from the ground.
@@ -436,6 +146,7 @@ export class GoogleMapsComponent implements OnInit {
     this.lat = myLatLng.lat;
     this.lng = myLatLng.lng;
   }
+
 
   /** ajust the size of the map depend on the device 
    * for mobile device like Android or iOS set the size to 100%
@@ -456,7 +167,7 @@ export class GoogleMapsComponent implements OnInit {
     }
   }
 
-  //TODO: use the zoom number to decide how match markers to show on the map
+
   /** this function fire each time the user zoom in or zoom out in the map
    * @param e the zoom level, 0 to 22, 0 is the whole world
    */
@@ -466,107 +177,6 @@ export class GoogleMapsComponent implements OnInit {
     this.mapsZoom = e;
   }
 
-  /** this function fire each time the currnet view is change
-   *  using the lat and lng of the view we can change the markers array
-   *  to display only the relevent marker to the currnet area in the world
-   * @param event the bounds of the current map view
-   */
-  boundsChange(event) {
-
-    //save the current map borders
-    this.mapBorders = {
-      "NorthEast":
-      {
-        "lat": event.getNorthEast().lat().toFixed(7),
-        "lng": event.getNorthEast().lng().toFixed(7)
-      },
-      "SouthWest":
-      {
-        "lat": event.getSouthWest().lat().toFixed(7),
-        "lng": event.getSouthWest().lng().toFixed(7)
-      }
-    };
-
-    //TODO: fix the if
-    // if (!_.valuesIn(this.mapBorders).some(x => { console.log(x); return ((x !== undefined) && (x != null)) })) {//make sure all the entries in mabBorders are full
-    _.forOwn(this.markersArray, (value, key) => {
-      // if ((value.length != 0) && (value !== null) && (value !== undefined)) {//check that the array has mrakers in it
-      if (this.arrayNotEmpty(value)) {
-        //filter out all the markers the not in the display rang
-        this[key] = _.filter(value, (item) => {
-          return (
-            (item.longitude <= this.mapBorders.NorthEast.lng) && (item.longitude >= this.mapBorders.SouthWest.lng)
-            &&
-            (item.latitude <= this.mapBorders.NorthEast.lat) && (item.latitude >= this.mapBorders.SouthWest.lat)
-          )
-        })
-      }
-    });
-    // }
-
-  }
-
-  //TODO: send the maps borders to the boundsChange() function to update the dispaly array
-  // mapReady(event) {
-  //   console.log("map is ready!");
-  //   console.log(event);
-  //   console.log("data object in mapReady")
-  //   console.log(this.jsonService.dataObject);
-  //   console.time("remove dup")
-  //   // let vale = this.jsonService.removeDup(this.jsonService.dataObject.heavy)
-  //   console.log("vale")
-  //   console.timeEnd("remove dup")
-
-  //   console.time("save");
-  //   this.saveMarkers();
-  //   console.timeEnd("save")
-  //   /*
-  //   console.log(event.getBounds().getNorthEast().lat());
-  //   const bounds: LatLngBounds = new google.maps.LatLngBounds();
-  //   console.log(bounds.getNorthEast().lat());
-  //   console.log(bounds.getSouthWest().lng());
-  //   // console.log(google.maps.getBounds());
-
-  //   console.log("in map ready:")
-  //   console.log(this.jsonService.dataObject);
-  //   this.saveDateLocally();
-  //   // this.boundsChange();
-  //   */
-  // }
-
-  /** this function reduce the number of markers in the main array
-   *  there for the google map can display the makrers easlly and quickly
-   *  using the big number role asure us that the number of each subject will be in the right %
-   * @param arr a large markers array
-   * @returns smaller marker array, with a const length
-   */
-  private getOnlyFew(arr: any): any {
-    let smallArr = [];
-    for (let i = 0; i < SMALL_ARRAY_CONST; i++) {
-      smallArr.push(
-        arr[Math.floor(Math.random() * arr.length)]
-      )
-    }
-    return smallArr;
-  }
-
-
-  /************************* WORKING FONCTIONS *******************/
-
-
-  /** save the markers from the json service to local arrays
-   *  use the fiter to save only the relevant arrays
-   *
-  saveMarkers(): any {
-    _.forOwn(this.jsonService.filterObject.filterBy, (value, key) => {
-      //remote the 'is' from 'isHeave'
-      let newKey = (key.includes("is")) ? key.slice(2).toLowerCase() : key;
-      //save to local arrays depending on the filter
-      if ((value) && (this.arrayNotEmpty(this.jsonService.dataObject[newKey]))) {
-        this.markersArray[newKey] = this.jsonService.dataObject[newKey].splice(0);
-      }
-    });
-  }*/
 
   /** check if the imput array is not empty
    * @param array 
