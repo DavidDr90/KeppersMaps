2e615e1476424397fe2b6b6386a29c6f0d74aef7
david <davidd.r.90@gmail.com>
2019-06-30 17:31:45 +0300

    documentation

--- FlaskServer/main.py
+++ FlaskServer/main.py
@@ -162,9 +162,8 @@ def distance(lat1, lon1, lat2, lon2):
     :param lon2:
     :return: the distance between the two locations
     """
-    p = 0.017453292519943295  # Pi/180
-    a = 0.5 - cos((lat2 - lat1) * p) / 2 + cos(lat1 * p) * \
-        cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
+    p = 0.017453292519943295  # Pi/180, use to convert from degrees to radians
+    a = 0.5 - cos((lat2 - lat1) * p) / 2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
     return 12742 * asin(sqrt(a))  # 2 * R; R = 6371 km
 
 
